4928405d2c0b3d48636ffc06f846cf2010419b30
WillDoyle <64522578+WillDoyle@users.noreply.github.com>
2023-10-12 23:23:58 +0200

    added icons, new images and updates to cafe

--- script.js
+++ script.js
@@ -205,6 +205,160 @@ function reveal() {
 
   const scrollContent = document.querySelector('.scroll-content');
 
+//   document.addEventListener("DOMContentLoaded", function () {
+//     const svgGraphic = document.getElementById("svg-graphic");
+//     const textDiv = document.querySelector(".text");
+//     const container = document.querySelector(".graphics__animation--container");
+//     const windowHeight = window.innerHeight;
+//     let isScrollingDown = true;
+//     let isSticky = false;
+
+//     // Set initial scale and position
+//     let scale = 0.5; // Initial scale factor
+//     svgGraphic.style.transform = `scale(${scale})`;
+//     svgGraphic.style.transition = "transform 0.5s ease-in-out, opacity 0.5s ease-in-out";
+
+//     window.addEventListener("scroll", () => {
+//         const scrollPosition = window.scrollY;
+
+//         if (scrollPosition >= container.offsetTop && scrollPosition < textDiv.offsetTop) {
+//             if (!isScrollingDown) {
+//                 isScrollingDown = true;
+//                 svgGraphic.style.transition = "transform 0.5s ease-in-out, opacity 0.5s ease-in-out";
+//             }
+
+//             // Scale the SVG as user scrolls down
+//             scale = 0.5 + ((scrollPosition - container.offsetTop) / windowHeight) * 0.5;
+//             scale = Math.min(1, scale); // Limit scale to 1
+//             svgGraphic.style.transform = `scale(${scale})`;
+
+//             // Fade in the SVG as it scales in
+//             const opacity = Math.min(1, (scrollPosition - container.offsetTop) / (windowHeight / 2));
+//             svgGraphic.style.opacity = opacity;
+//         } else if (scrollPosition >= textDiv.offsetTop) {
+//             // Keep the scaled SVG sticky
+//             svgGraphic.style.transition = "none"; // Remove transition
+//             svgGraphic.style.position = "fixed";
+//             svgGraphic.style.top = "200px";
+//             svgGraphic.style.left = "50%"; // Center horizontally
+//             svgGraphic.style.transformOrigin = "center bottom"; // Anchor at the bottom
+//             isSticky = true;
+//         } else if (isSticky) {
+//             // Revert to initial state if scrolling up after sticky
+//             svgGraphic.style.position = "static";
+//             svgGraphic.style.transform = "scale(0.5)";
+//             svgGraphic.style.opacity = 0;
+//             svgGraphic.style.transition = "transform 0.5s ease-in-out, opacity 0.5s ease-in-out";
+//             isSticky = false;
+//         } else {
+//             // User is scrolling up; scale and fade out the SVG
+//             if (isScrollingDown) {
+//                 isScrollingDown = false;
+//                 svgGraphic.style.transition = "transform 0.5s ease-in-out, opacity 0.5s ease-in-out";
+//             }
+//             scale = 1 - ((scrollPosition - textDiv.offsetTop) / windowHeight);
+//             scale = Math.max(0.5, scale);
+//             svgGraphic.style.transform = `scale(${scale})`;
+//             svgGraphic.style.opacity = scale;
+//         }
+//     });
+// });
+
+
+// Get a reference to the div with the ID "hoverDiv"
+const coffeeID = document.getElementById("coffeeid");
+const energybarsID = document.getElementById("energyid");
+const icecreamID = document.getElementById("icecreamid");
+
+
+const coffeeH1 = document.querySelector('#coffee h1');
+const energyH1 = document.querySelector('#energybars h1');
+const icecreamH1 = document.querySelector('#icecream h1');
+
+
+console.log(coffeeH1);
+
+// Get a reference to the "fuel__img" div
+const fuelImg = document.querySelector(".fuel__img");
+
+// Function to change the background image when hovering
+function changeBackgroundImage(id) {
+  console.log(id);
+  if(id === "coffee"){
+    fuelImg.style.backgroundImage = "url('./assets/coffee.jpeg')"; // Change 'new-image.jpg' to the desired image URL
+    coffee.style.backgroundColor = "#9facf6";
+    coffeeH1.style.color = "#242424";
+    coffeeID.style.color = "#242424";
+  
+    coffeeID.style.display = "block";
+    energybarsID.style.display = "none";
+    icecreamID.style.display = "none";
+    
+    
+
+   
+  }
+  
+  if(id==="energybars"){
+    fuelImg.style.backgroundImage = "url('./assets/cliff-bar.webp')";
+    energybars.style.backgroundColor = "#fe464f";
+
+    coffeeID.style.display = "none";
+    energybarsID.style.display = "block";
+    icecreamID.style.display = "none";
+    
+  }
+
+  if(id === "icecream")
+  {
+    fuelImg.style.backgroundImage = "url('./assets/pat-and-stick.png')";
+
+    icecream.style.backgroundColor = "#d3f258";
+    icecreamH1.style.color = "#242424";
+    icecreamID.style.color = "#242424";
+
+    coffeeID.style.display = "none";
+    energybarsID.style.display = "none";
+    icecreamID.style.display = "block";
+
+
+
+    
+    
+
+  }
+}
+
+// Function to reset the background image when not hovering
+function resetBackgroundImage() {
+   // Change 'original-image.jpg' to the default image URL
+   
+   coffee.style.backgroundColor = "";
+   icecream.style.backgroundColor = "";
+   energybars.style.backgroundColor = "";
+   fuelImg.style.backgroundImage = "url(./assets/cafe-2.jpg";
+
+
+
+   coffeeH1.style.color = "#fff";
+   icecreamH1.style.color = "#fff";
+   energyH1.style.color = "#fff";
+
+   energybarsID.style.display = "none";
+   coffeeID.style.display = "none";
+   icecreamID.style.display = "none";
+
+
+   
+}
+
+
+
+
+
+
+
+
 function cafeScroll(){
   if(location.pathname=="/cafe.html"){
     const scrollY = window.scrollY;
